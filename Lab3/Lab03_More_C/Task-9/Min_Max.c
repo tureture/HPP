2d76254ccb691c30cf552aa21ecd67e65a910626
Ture Hassler <ture.hassler@gmail.com>
2023-01-28 22:55:53 +0100

    Task 9 done

--- Lab3/Lab03_More_C/Task-9/Min_Max.c
+++ Lab3/Lab03_More_C/Task-9/Min_Max.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/time.h>
 
 
 double** allocate_matrix(int n);
@@ -8,13 +9,17 @@ void fill_matrix(double** theMatrix, int n);
 void print_matrix(double** theMatrix, int n);
 double get_min_value(double** theMatrix, int n);
 double get_max_value(double** theMatrix, int n);
+double get_wall_seconds();
 
 int main()
 {
+
   int n;
   printf("\nEnter the dimension for a square matrix:");
   scanf("%d",&n);
   printf("n = %d\n", n);
+
+  double start = get_wall_seconds();
   double** matrixA = allocate_matrix(n);
   fill_matrix(matrixA, n);
   double minValue = get_min_value(matrixA, n);
@@ -22,6 +27,14 @@ int main()
   //print_matrix(matrixA, n);
   printf("Min value: %14.9f  Max value: %14.9f\n", minValue, maxValue);
   deallocate_matrix(matrixA, n);
+
+
+  double** matrixB = allocate_matrix(n);
+  deallocate_matrix(matrixB,n);
+
+  double end = get_wall_seconds();
+  printf("Time elapsed: %f seconds", end - start);
+
   return 0;
 }
 
@@ -85,5 +98,12 @@ double get_min_value(double** theMatrix, int n)
       if(min > theMatrix[i][j])
 	min = theMatrix[i][j];
   return min;
+}
+
+double get_wall_seconds(){
+struct timeval tv;
+gettimeofday(&tv, NULL);
+double seconds = tv.tv_sec + (double)tv.tv_usec/1000000;
+return seconds;
 }
 
