298026e996a03e35ed7ddfcdf39c0f4f4998971b
Ture Hassler <ture.hassler@gmail.com>
2023-02-12 12:48:35 +0100

    Task 1 done

--- Lab8/Lab08_Pthreads_I/Task-1/threadtest1.c
+++ Lab8/Lab08_Pthreads_I/Task-1/threadtest1.c
@@ -3,6 +3,25 @@
 
 void* the_thread_func(void* arg) {
   /* Do something here? */
+  
+  printf("threadfunc starting doing some work.\n");
+  long int i;
+  double sum;
+  for(i = 0; i < 10000000; i++)
+    sum += 1e-5;
+  printf("Result of work in threadfunc: sum = %f\n", sum);
+  return NULL;
+}
+
+void* the_thread_func_B(void* arg) {
+  /* Do something here? */
+  
+  printf("threadfunc B starting doing some work.\n");
+  long int i;
+  double sum;
+  for(i = 0; i < 10000000; i++)
+    sum += 1e-5;
+  printf("Result of work in threadfunc B: sum = %f\n", sum);
   return NULL;
 }
 
@@ -11,16 +30,25 @@ int main() {
 
   /* Start thread. */
   pthread_t thread;
+  pthread_t thread_B;
   printf("the main() function now calling pthread_create().\n");
   pthread_create(&thread, NULL, the_thread_func, NULL);
+  pthread_create(&thread_B,  NULL, the_thread_func_B, NULL);
 
   printf("This is the main() function after pthread_create()\n");
 
   /* Do something here? */
+  printf("main() starting doing some work.\n");
+  long int i;
+  double sum;
+  for(i = 0; i < 100000000; i++)
+    sum += 1e-7;
+  printf("Result of work in main(): sum = %f\n", sum);
 
   /* Wait for thread to finish. */
   printf("the main() function now calling pthread_join().\n");
   pthread_join(thread, NULL);
+  pthread_join(thread_B, NULL);
 
   return 0;
 }
