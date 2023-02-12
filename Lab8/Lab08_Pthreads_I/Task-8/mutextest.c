7e178f670143a0d3130c1a9077f2d1f6b03da1b6
Ture Hassler <ture.hassler@gmail.com>
2023-02-12 17:43:41 +0100

    Task 8 done

--- Lab8/Lab08_Pthreads_I/Task-8/mutextest.c
+++ Lab8/Lab08_Pthreads_I/Task-8/mutextest.c
@@ -3,12 +3,19 @@
 #include <pthread.h>
 
 long int sum = 0;
-int N = 100000;
+pthread_mutex_t mutex;
 
 void* the_thread_func(void* arg) {
+  int N = 10000;
 
-  for(int i = 1; i <= N; ++i)
-  	sum += 1;
+  
+  for(int i = 1; i <= N; ++i){
+    pthread_mutex_lock(&mutex);
+    sum += i;
+    pthread_mutex_unlock(&mutex);
+  }
+    
+    
 
   return NULL;
 }
@@ -18,6 +25,8 @@ int main(int argc, char **argv) {
   if(argc != 2) {printf("Usage: %s N\n", argv[0]); return -1; }
 
   printf("This is the main() function starting.\n");
+
+  pthread_mutex_init(&mutex, NULL);
 
   int N = atoi(argv[1]);
 
@@ -34,6 +43,8 @@ int main(int argc, char **argv) {
   printf("the main() function now calling pthread_join().\n");
   for(int i = 0; i < N; i++)
     pthread_join(threads[i], NULL);
+
+  pthread_mutex_destroy(&mutex);
 
   printf("sum = %ld\n", sum); 
 
