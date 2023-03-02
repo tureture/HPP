fe59cb15778657f37aba3de61db70cc7957791d1
Ture Hassler <ture.hassler@gmail.com>
2023-03-02 21:24:49 +0100

    more runs + updated makefile

--- Assignment4/runtimes.sh
+++ Assignment4/runtimes.sh
@@ -6,11 +6,11 @@ do
 done
 for T in $(seq 18 2 32)
 do
-        echo "N = $T"
+        echo "T = $T"
         time ./galsim 1000 input_data/ellipse_N_01000.gal 100 0.00001 0 $T
 done
 for T in $(seq  48 12 128)
 do
-        echo "N = $T"
+        echo "T = $T"
         time ./galsim 1000 input_data/ellipse_N_01000.gal 100 0.00001 0 $T
 done
