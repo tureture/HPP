e25e0cd1a5bf8726fc1772fb51fd0869660a5c94
Ture Hassler <ture.hassler@gmail.com>
2023-03-04 23:15:20 +0100

    created new folder for debugging assignments 2 & 4

---
+++ Debug_Assignments/A4/Openmp/graphics/galsim.c
@@ -0,0 +1,187 @@
+#include "graphics/graphics.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <omp.h>
+
+/*
+Assignment 4 - OpenMP version of the code
+High Performance Computing
+By Ture Hassler & Jacob Malmenstedt
+*/
+
+// Define a struct for the particle
+struct Particle
+{
+    double x;
+    double y;
+    double mass;
+    double vx;
+    double vy;
+    double brightness;
+};
+
+int main(int argc, char *argv[])
+{
+
+    // Parse command line arguments and initialize input variables
+    if (argc != 7)
+    {
+        printf("Usage: ./galsim N filename nsteps delta_t graphics num_threads\n");
+        return 1;
+    }
+    int N = atoi(argv[1]);
+    char *filename = argv[2];
+    int nsteps = atoi(argv[3]);
+    double delta_t = atof(argv[4]);
+    int graphics = atoi(argv[5]);
+    int nr_threads = atoi(argv[6]);
+
+    omp_set_num_threads(nr_threads);
+
+    // Allocate memory for N particles
+    struct Particle *particles = (struct Particle *)malloc(N * sizeof(struct Particle));
+
+    // initialize variables inside for loops
+    double acc_x = 0, acc_y = 0, acc_k; // acceleration
+    double rij;                         // distance between particles
+    const double e0 = 0.001;            // weird constant
+    const double G = 100.0 / N;         // gravitational constant
+    int k;
+    struct Particle p1, p2; // particles
+
+    // Read data from file and initialize particles with data
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += fread(&particles[i].x, sizeof(double), 1, file);
+        sum += fread(&particles[i].y, sizeof(double), 1, file);
+        sum += fread(&particles[i].mass, sizeof(double), 1, file);
+        sum += fread(&particles[i].vx, sizeof(double), 1, file);
+        sum += fread(&particles[i].vy, sizeof(double), 1, file);
+        sum += fread(&particles[i].brightness, sizeof(double), 1, file);
+    }
+    fclose(file);
+    if (sum != 6 * N) // check if all data was read
+    {
+        printf("Error reading file\n");
+        return 1;
+    }
+
+    // *********************** Simulation with graphics ***************************************. Note not paralleized! 
+
+    // Initialize graphics, separate if else statement to avoid unnecessary calculations
+    if (graphics)
+    {
+        InitializeGraphics("Galaxy Simulation", 800, 800);
+        SetCAxes(0, 1);
+        printf("Hit q to quit.\n");
+        while (!CheckForQuit()) // note this will run until you quit. Working as intended.
+        {
+            for (int j = 0; j < N; j++) // for all particles update acc and vel
+            {
+
+                p1 = particles[j]; // current particle
+
+                
+                for (int k = 0; k < N; k++) // calculations of acc for all particles before current particle
+                {
+                    p2 = particles[k];
+                    rij = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+                    acc_k = p2.mass / ((rij + e0) * (rij + e0) * (rij + e0));
+                    acc_x += acc_k * (p1.x - p2.x);
+                    acc_y += acc_k * (p1.y - p2.y);
+                }
+                acc_x *= -G;
+                acc_y *= -G;
+
+                // update velocity
+                particles[j].vx += acc_x * delta_t;
+                particles[j].vy += acc_y * delta_t;
+
+                // reset acceleration
+                acc_x = 0;
+                acc_y = 0;
+            }
+            for (int j = 0; j < N; j++) // for all particles update pos
+            {
+                // update position
+                particles[j].x += particles[j].vx * delta_t;
+                particles[j].y += particles[j].vy * delta_t;
+            }
+        }
+        FlushDisplay();
+        CloseDisplay();
+    }
+    //********************** No Graphics Simulation ************************************************************
+    else
+    {
+        for (int i = 0; i < nsteps; i++) // for all timesteps
+        {
+            #pragma omp parallel for private(p1, p2, rij, acc_k, acc_x, acc_y, k) schedule(dynamic, 4)
+            for (int j = 0; j < N; j++) // for all particles update acc and vel
+            {
+                p1 = particles[j]; // current particle
+
+                for (k = 0; k < N; k++) // calculations of acc for all particles
+                {
+                    p2 = particles[k];
+                    rij = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+                    acc_k = p2.mass / ((rij + e0) * (rij + e0) * (rij + e0));
+                    acc_x += acc_k * (p1.x - p2.x);
+                    acc_y += acc_k * (p1.y - p2.y);
+                }
+                acc_x *= -G;
+                acc_y *= -G;
+
+                // update velocity
+                particles[j].vx += acc_x * delta_t;
+                particles[j].vy += acc_y * delta_t;
+
+                // reset acceleration
+                acc_x = 0;
+                acc_y = 0;
+            }
+            #pragma omp barrier // wait for all threads to finish (maybe not necessary)
+            // the implied barrier should work but to force the same threads to continue I added this
+
+
+            #pragma omp parallel for schedule(static)
+            //#pragma omp target data map(tofrom: particles[0:N])
+            //#pragma omp target teams distribute parallel for
+            for (int j = 0; j < N; j++) // for all particles update pos
+            {
+                // update position
+                particles[j].x += particles[j].vx * delta_t;
+                particles[j].y += particles[j].vy * delta_t;
+            }
+        }
+    }
+
+    // Write to binary file
+    FILE *file_out;
+    file_out = fopen("result.gal", "w");
+    for (int i = 0; i < N; i++)
+    {
+        fwrite(&particles[i].x, sizeof(double), 1, file_out);
+        fwrite(&particles[i].y, sizeof(double), 1, file_out);
+        fwrite(&particles[i].mass, sizeof(double), 1, file_out);
+        fwrite(&particles[i].vx, sizeof(double), 1, file_out);
+        fwrite(&particles[i].vy, sizeof(double), 1, file_out);
+        fwrite(&particles[i].brightness, sizeof(double), 1, file_out);
+    }
+    fclose(file_out);
+
+    // free memory
+    free(particles);
+
+    return 0;
+}
---
+++ Debug_Assignments/A4/Pthreads/galsim.c
@@ -0,0 +1,319 @@
+#include "graphics/graphics.h"
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <pthread.h>
+
+/*
+Assignment 4 - Parallelization with Pthreads
+High Performance Computing
+By Ture Hassler & Jacob Malmenstedt
+*/
+
+// Global variables
+int N;
+int nsteps;
+double delta_t;
+double e0 = 0.001;
+double G;
+struct Particle *particles;
+
+// Pthread stuff
+int NUM_THREADS;
+pthread_mutex_t lock;
+pthread_cond_t mysignal;
+int waiting = 0;
+int state = 0;
+
+pthread_mutex_t lock_outer;
+pthread_cond_t mysignal_outer;
+int waiting_outer = 0;
+int state_outer = 0;
+
+// Define a struct for the particle
+struct Particle
+{
+    double x;
+    double y;
+    double mass;
+    double vx;
+    double vy;
+    double brightness;
+    double megasum;
+};
+
+// Input data for pthreads
+struct Pthread_data
+{
+    int lowerB;
+    int upperB;
+};
+
+// Borrowed from Lab9/Task-4
+void barrier_inner()
+{
+    int mystate;
+    pthread_mutex_lock(&lock);
+    mystate = state;
+    waiting++;
+    if (waiting == NUM_THREADS)
+    {
+        waiting = 0;
+        state = 1 - mystate;
+        pthread_cond_broadcast(&mysignal);
+    }
+    while (mystate == state)
+    {
+        pthread_cond_wait(&mysignal, &lock);
+    }
+    pthread_mutex_unlock(&lock);
+}
+
+// Borrowed from Lab9/Task-4
+void barrier_outer()
+{
+    int mystate;
+    pthread_mutex_lock(&lock_outer);
+    mystate = state_outer;
+    waiting_outer++;
+    if (waiting_outer == NUM_THREADS)
+    {
+        waiting_outer = 0;
+        state_outer = 1 - mystate;
+        pthread_cond_broadcast(&mysignal_outer);
+    }
+    while (mystate == state_outer)
+    {
+        pthread_cond_wait(&mysignal_outer, &lock_outer);
+    }
+    pthread_mutex_unlock(&lock_outer);
+}
+
+void *calc_forces(void *arg)
+{
+    /* Calc forces for one specific particle */
+    struct Pthread_data *input = (struct Pthread_data *)arg;
+
+    int lb = input->lowerB;
+    int ub = input->upperB;
+
+    double acc_x = 0, acc_y = 0, acc_k; // acceleration
+    double rij;                         // distance between particles
+    struct Particle p1, p2;             // particles
+
+    for (int i = 0; i < nsteps; i++)
+    {
+        for (int j = lb; j < ub; j++) // calculations for all particles between lb and ub
+        {
+            p1 = particles[j]; // current particle
+
+            for (int k = 0; k < N; k++) // calculations of acc for all particles acting on current particle
+            {
+                p2 = particles[k];
+                rij = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+                acc_k = p2.mass / ((rij + e0) * (rij + e0) * (rij + e0));
+                acc_x += acc_k * (p1.x - p2.x);
+                acc_y += acc_k * (p1.y - p2.y);
+            }
+            acc_x *= -G;
+            acc_y *= -G;
+
+            // update velocity
+            particles[j].vx += acc_x * delta_t;
+            particles[j].vy += acc_y * delta_t;
+
+            // reset acceleration
+            acc_x = 0;
+            acc_y = 0;
+        }
+        // Wait for all threads to reach the barrier to sync before updating pos
+        barrier_inner();
+
+        for (int j = lb; j < ub; j++) // update pos
+        {
+            // update position
+            particles[j].x += particles[j].vx * delta_t;
+            particles[j].y += particles[j].vy * delta_t;
+        }
+        barrier_outer(); // Wait for all threads to reach the barrier to sync between timesteps
+    }
+    return NULL;
+}
+
+int main(int argc, char *argv[])
+{
+    // Parse command line arguments and initialize input variables
+    if (argc != 7)
+    {
+        printf("Usage: ./galsim N filename nsteps delta_t graphics num_threads\n");
+        return 1;
+    }
+
+    N = atoi(argv[1]);
+    char *filename = argv[2];
+    nsteps = atoi(argv[3]);
+    delta_t = atof(argv[4]);
+    int graphics = atoi(argv[5]);
+    NUM_THREADS = atoi(argv[6]);
+
+    // Allocate memory for N particles
+    particles = (struct Particle *)malloc(N * sizeof(struct Particle));
+
+    // initialize variables inside for loops
+    double acc_x = 0, acc_y = 0, acc_k; // acceleration
+    double rij;                         // distance between particles
+    e0 = 0.001;                         // weird constant
+    G = 100.0 / (double)N;              // gravitational constant
+    int k;
+    struct Particle p1, p2; // particles
+
+    // Pthread stuff
+    pthread_t threads[NUM_THREADS];
+    struct Pthread_data *data = (struct Pthread_data *)malloc((NUM_THREADS + 1) * sizeof(struct Pthread_data));
+
+    pthread_cond_init(&mysignal, NULL);
+    pthread_mutex_init(&lock, NULL);
+
+    pthread_cond_init(&mysignal_outer, NULL);
+    pthread_mutex_init(&lock_outer, NULL);
+
+    // Read data from file and initialize particles with data
+    FILE *file = fopen(filename, "r");
+    if (file == NULL)
+    {
+        printf("Error opening file\n");
+        return 1;
+    }
+
+    int sum = 0;
+    for (int i = 0; i < N; i++)
+    {
+        sum += fread(&particles[i].x, sizeof(double), 1, file);
+        sum += fread(&particles[i].y, sizeof(double), 1, file);
+        sum += fread(&particles[i].mass, sizeof(double), 1, file);
+        sum += fread(&particles[i].vx, sizeof(double), 1, file);
+        sum += fread(&particles[i].vy, sizeof(double), 1, file);
+        sum += fread(&particles[i].brightness, sizeof(double), 1, file);
+    }
+    fclose(file);
+    if (sum != 6 * N) // check if all data was read
+    {
+        printf("Error reading file\n");
+        return 1;
+    }
+
+    // *********************** Simulation with graphics ***************************************
+
+    // Initialize graphics, separate if else statement to avoid unnecessary calculations. Note this part is not parallelized!
+    if (graphics)
+    {
+        InitializeGraphics("Galaxy Simulation", 800, 800);
+        SetCAxes(0, 1);
+        printf("Hit q to quit.\n");
+        while (!CheckForQuit()) // note this will run until you quit. Working as intended.
+        {
+            for (int j = 0; j < N; j++) // for all particles update acc and vel
+            {
+
+                p1 = particles[j]; // current particle
+
+                for (k = 0; k < j; k++) // calculations of acc for all particles before current particle
+                {
+
+                    p2 = particles[k];
+                    rij = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+                    acc_k = p2.mass / ((rij + e0) * (rij + e0) * (rij + e0));
+                    acc_x += acc_k * (p1.x - p2.x);
+                    acc_y += acc_k * (p1.y - p2.y);
+                }
+
+                for (k = j + 1; k < N; k++) // calc for all particles after current particle
+                {
+                    p2 = particles[k];
+                    rij = sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+                    acc_k = p2.mass / ((rij + e0) * (rij + e0) * (rij + e0));
+                    acc_x += acc_k * (p1.x - p2.x);
+                    acc_y += acc_k * (p1.y - p2.y);
+                }
+                acc_x *= -G;
+                acc_y *= -G;
+
+                // update velocity
+                particles[j].vx += acc_x * delta_t;
+                particles[j].vy += acc_y * delta_t;
+
+                // reset acceleration
+                acc_x = 0;
+                acc_y = 0;
+            }
+
+            for (int j = 0; j < N; j++) // for all particles update pos
+            {
+                // update position
+                particles[j].x += particles[j].vx * delta_t;
+                particles[j].y += particles[j].vy * delta_t;
+            }
+        }
+        FlushDisplay();
+        CloseDisplay();
+    }
+    //********************** No Graphics Simulation *******************************************
+    else
+    {
+        // Distribute work evenly between threads
+        int remainder = N % NUM_THREADS;
+        int tasks_per_thread = N / NUM_THREADS;
+        int tmp = 0;
+
+        for (int j = 0; j < NUM_THREADS; j++) // create threads and distribute work evenly
+        {
+            if (j < remainder)
+            {
+                data[j].lowerB = tmp;
+                tmp += tasks_per_thread + 1;
+                data[j].upperB = tmp;
+            }
+            else
+            {
+                data[j].lowerB = tmp;
+                tmp += tasks_per_thread;
+                data[j].upperB = tmp;
+            }
+            pthread_create(&threads[j], NULL, calc_forces, (void *)&data[j]);
+        }
+
+        for (int j = 0; j < NUM_THREADS; j++) // join threads
+        {
+            pthread_join(threads[j], NULL);
+        }
+    }
+
+
+    // Write to binary file
+    FILE *file_out;
+    file_out = fopen("result.gal", "w");
+    for (int i = 0; i < N; i++)
+    {
+        // printf("%f %f %f %f %f %f \n", particles[i].x, particles[i].y, particles[i].mass, particles[i].vx, particles[i].vy, particles[i].brightness);
+        fwrite(&particles[i].x, sizeof(double), 1, file_out);
+        fwrite(&particles[i].y, sizeof(double), 1, file_out);
+        fwrite(&particles[i].mass, sizeof(double), 1, file_out);
+        fwrite(&particles[i].vx, sizeof(double), 1, file_out);
+        fwrite(&particles[i].vy, sizeof(double), 1, file_out);
+        fwrite(&particles[i].brightness, sizeof(double), 1, file_out);
+    }
+    fclose(file_out);
+
+    // free memory
+    free(particles);
+
+    // Cleanup
+    pthread_cond_destroy(&mysignal);
+    pthread_mutex_destroy(&lock);
+
+    pthread_cond_destroy(&mysignal_outer);
+    pthread_mutex_destroy(&lock_outer);
+
+    return 0;
+}
