3ba9bc1caa4aedfa170194f51ee326b0a631d9f1
Ture Hassler <ture.hassler@gmail.com>
2023-03-16 22:47:32 +0100

    working version randomizing the lists

--- Project/sudoku_openmp_test.c
+++ Project/sudoku_openmp_test.c
@@ -2,9 +2,7 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <string.h>
-#ifdef _OPENMP
 #include <omp.h>
-#endif
 
 /*
 High Performance Programming
@@ -13,80 +11,118 @@ by Ture Hassler
 */
 
 // function headers
-unsigned int validateBoard(unsigned int coordinates, unsigned int num, unsigned int ** board, unsigned int n, unsigned int N);
-unsigned int solveBoard(unsigned int ** board_copy, unsigned int n, unsigned int N, unsigned int nr_remaining, unsigned int * unassigned_indicies);
-void print_board(unsigned int ** board, unsigned int n, unsigned int N);
-void write_board(unsigned int ** board, unsigned int N, char * output);
+int validateBoard(int coordinates,  int num,  int ** board,  int n,  int N); 
+int solveBoard( int ** board,  int n,  int N,  int nr_remaining,  int * unassigned_indicies, int depth);
+void print_board( int ** board,  int n,  int N);
+void write_board( int ** board,  int N, char * output);
+int solveBoard_serial(int ** board,  int n,  int N,  int nr_remaining,  int * unassigned_indicies, int depth);
+void shuffle(int *array, size_t n);
+
+int solution_found = 0;
+
 
 int main(int argc, char *argv[]){
 
     // Parse command line arguments and initialize input variables
-    if (argc != 4){
-        printf("Usage: ./sudoku n input_filename output_filename \n");
+    if (argc != 5){
+        printf("Usage: ./sudoku n input_filename output_filename nr_threads \n");
         return 1;
     }
 
     int n = atoi(argv[1]);
     char *filename_in= argv[2];
     // char *filename_out = argv[3];
+    int NUM_THREADS = atoi(argv[4]);
 
     int N = n * n; // Size of board
 
-    // Allocate memory for board of size n^2 x n^2
-    unsigned int ** board = (unsigned int **)malloc(N * sizeof(unsigned int *));
-    for (int i = 0; i < N; i++){
-        board[i] = (unsigned int *)malloc(N * sizeof(unsigned int));
+    // Allocate memory for NUM_THREADS boards 
+    int *** boards = malloc(NUM_THREADS * sizeof(int **));
+    for (int i = 0; i < NUM_THREADS; i++){
+        boards[i] = malloc(N * sizeof(int *));
+        for (int j = 0; j < N; j++){
+            boards[i][j] = malloc(N * sizeof(int));
+        }
     }
+
 
     // Read data from file and initialize board from input file
     FILE *file = fopen(filename_in, "r");
     if (file == NULL){
         printf("Error opening file\n");
         return 1;
-    }
+    }   
+
+    // After file reead
+    printf("File read\n");
+
 
     // Loops through data twice, first to count number of unassigned values and fill upp the board
     // Then to assign them to an array
     int unnasigned_n = 0;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
-            fscanf(file, "%d", &board[i][j]);
-            if (board[i][j] == 0){
+            int val;
+            fscanf(file, "%d", &val);
+            if (boards[0][i][j] == 0){
+                for(int k = 0; k < NUM_THREADS; k++){
+                    boards[k][i][j] = val;
+                }
                 unnasigned_n++;
             }
         }
     } 
 
-    unsigned int * unassigned_indicies = (unsigned int *)malloc((unnasigned_n + 1) * sizeof(unsigned int));
+    // allocate memory for unassigned indicies
+    int ** unnasigned_indicies = malloc(NUM_THREADS * sizeof(int *));
+    for (int i = 0; i < NUM_THREADS; i++){
+        unnasigned_indicies[i] = malloc(unnasigned_n * sizeof(int));
+    }
+
     unnasigned_n = 0;
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
-            if (board[i][j] == 0){
-                unassigned_indicies[unnasigned_n] = i * N + j;
+            if (boards[0][i][j] == 0){
+                for (int k = 0; k < NUM_THREADS; k++){
+                    unnasigned_indicies[k][unnasigned_n] = i * N + j;
+                }       
                 unnasigned_n++;
             }
         }
     }
 
-    printf("Omp cancellation enabled: %d \n", omp_get_cancellation());
-    #pragma omp parallel default(none) num_threads(4) shared(board, n, N, unnasigned_n, unassigned_indicies)
-    {   
-        #pragma omp single
-        {
-            solveBoard(board, n, N, unnasigned_n, unassigned_indicies);
+    // Shuffle unnasigned indicies list
+    for (int i = 1; i < NUM_THREADS; i++){
+        shuffle(unnasigned_indicies[i], unnasigned_n);
+    }
+
+
+    // Print openmp stuff
+    printf("Number of threads: %d \n", NUM_THREADS);
+    printf("max threads: %d \n", omp_get_max_threads());
+
+    
+    // Solve board
+    #pragma omp parallel 
+    {
+        #pragma omp taskloop num_tasks(NUM_THREADS) firstprivate(boards, n, N, unnasigned_n, unnasigned_indicies)
+        for (int i=0; i < NUM_THREADS; i++){
+        solveBoard_serial(boards[i], n, N, unnasigned_n, unnasigned_indicies[i], 0);   
         }
+
     }
     
-     
+    printf("Done \n");
+
     return 0;
 }
 
-unsigned int validateBoard(unsigned int coordinates, unsigned int num, unsigned int ** board, unsigned int n, unsigned int N){
+ int validateBoard( int coordinates,  int num, int ** board,  int n,  int N){
     // Get row and column from single coordinate number (row * N + col)
-    unsigned int row = coordinates / N;
-    unsigned int col = coordinates % N;
-    unsigned int box_x = (row / n) * n;
-    unsigned int box_y = (col / n) * n;
+     int row = coordinates / N;
+     int col = coordinates % N;
+     int box_x = (row / n) * n;
+     int box_y = (col / n) * n;
 
     // Check row
     for (int i = 0; i < N; i++){
@@ -96,7 +132,7 @@ unsigned int validateBoard(unsigned int coordinates, unsigned int num, unsigned 
     }
 
     // Check column
-    for (int i = 0; i < n; i++){
+    for (int i = 0; i < N; i++){
         if (board[i][col] == num){
             return 0;
         }
@@ -114,57 +150,47 @@ unsigned int validateBoard(unsigned int coordinates, unsigned int num, unsigned 
     return 1;
 }
     
-unsigned int solveBoard(unsigned int ** board_copy, unsigned int n, unsigned int N, unsigned int nr_remaining, unsigned int * unassigned_indicies){
+
+ int solveBoard_serial(int ** board,  int n,  int N,  int nr_remaining,  int * unassigned_indicies, int depth){
     
-    unsigned int row, col;
-    int solution_found = 0;
 
-    // Allocate memory for board of size n^2 x n^2
-    unsigned int ** board = (unsigned int **)malloc(N * sizeof(unsigned int *));
-    for (int i = 0; i < N; i++){
-        board[i] = (unsigned int *)malloc(N * sizeof(unsigned int));
-    }
-
-    // Copy board
-    for(int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            board[i][j] = board_copy[i][j];
-        }
-    }
-
+    #pragma omp flush (solution_found)
     if (solution_found){
+        printf("Solution found in another thread \n");
         return 1;
     }
 
-    if (nr_remaining == 0) {
-        write_board(board, N, "output.txt");
-        print_board(board, n, N);
+    int row, col;
+
+    if (nr_remaining == 0){
+        #pragma omp critical 
+        {
+            printf("Solution found \n");
+            write_board(board, N, "output.txt");
+            print_board(board, n, N);
+            solution_found = 1;
+        }
         return 1;
-    } else {
-        unsigned int coordinates = unassigned_indicies[nr_remaining - 1];
+    }
+    else {
+         int coordinates = unassigned_indicies[nr_remaining -1];
         row = coordinates / N;
         col = coordinates % N;
         
-        int solution_found = 0;
-        #pragma omp sections
-            for (int i = 1; i <= N && !solution_found; i++) {
-                #pragma omp section
-                {
-                    if (validateBoard(coordinates, i, board, n, N)) {
-                    board[row][col] = i;  
-                    solveBoard(board, n, N, nr_remaining - 1, unassigned_indicies);
-                    board[row][col] = 0;
+        for (int i = 1; i <= N; i++){
+            if (validateBoard(coordinates, i, board, n, N)){
+                board[row][col] = i;  
+                if (solveBoard_serial(board, n, N, nr_remaining - 1, unassigned_indicies, depth + 1)){
+                    return 1;
                 }
-
-                }
-
-            } 
-        
-        return solution_found;
-    }  
+                board[row][col] = 0;
+            }
+        }     
+    }   
+    return 0;
 }
 
-void print_board(unsigned int ** board, unsigned int n, unsigned int N){
+void print_board(int ** board, int n, int N){
     for (int i = 0; i < N; i++){
         for (int j = 0; j < N; j++){
             if (j % n == 0 && j != 0){
@@ -183,7 +209,7 @@ void print_board(unsigned int ** board, unsigned int n, unsigned int N){
     printf("\n");
 }
 
-void write_board(unsigned int ** board, unsigned int N, char * output){
+void write_board(int ** board, int N, char * output){
     FILE *file = fopen(output, "w");
     if (file == NULL){
         printf("Error opening file\n");
@@ -195,5 +221,20 @@ void write_board(unsigned int ** board, unsigned int N, char * output){
             fprintf(file, "%d ", board[i][j]);
         }
         fprintf(file, "\n");
+    }
+}
+
+void shuffle(int *array, size_t n)
+{
+    if (n > 1) 
+    {
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          int t = array[j];
+          array[j] = array[i];
+          array[i] = t;
+        }
     }
 }
